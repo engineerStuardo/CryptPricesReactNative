01b5fa65500c4fb093fd2569936658aadabff3f6
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-03-21 05:39:57 -0600

    installing and configuring react navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>GoodBye world!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
