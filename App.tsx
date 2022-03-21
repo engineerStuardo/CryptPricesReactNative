5b46bfd4b59c179d6f5a8a301ba48ef60b8f469a
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-03-21 07:10:53 -0600

    installing babel-plugin-module-resolver so we can configure babel to use absolute paths, also create Home component and start using absolute

--- App.tsx
+++ App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
+import Home from '@screens/Home';
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
